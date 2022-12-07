71262ac4125882384a6ad277c5cbe0c8056ed0cd
micksuffolk <suffolk.mike@gmail.com>
2022-12-07 20:17:45 +0000

    MPLABX Blank project created for PIC16F887.

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=DC_Boost_Converter_ResDev.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/DC_Boost_Converter_ResDev.X.production.hex
